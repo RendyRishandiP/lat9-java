b2483591a85b3de6bdfc7511cf24a9c79766a851
RendyRishandiP <rrperkasa@gmail.com>
2019-11-21 20:07:55 +0700

    lat9-java

---
+++ 1.Menampilkan Data Diri/src/l1/menampilkan/data/diri/L1MenampilkanDataDiri.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package l1.menampilkan.data.diri;
+
+/**
+ * NAMA  : Rendy Rishandi Perkasa
+ * NIM   : A2.1900147
+ * Kelas : TI-1C
+ * 
+ * @author RendyRishandiPerkasa
+ */
+public class L1MenampilkanDataDiri {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Biodata" + "\n" + "-----------------" );
+        System.out.println("NAMA\t\t         : Rendy Rishandi Perkasa");
+        System.out.println("ALAMAT\t\t         : Bantarujeg Kab.majalengka ");
+        System.out.println("TEMPAT LAHIR\t\t : Majalengka ");
+        System.out.println("TANGGAL LAHIR\t\t : 03 Mei 1996");
+        System.out.println("--------------------------");
+        System.out.println("HOBI\t\t: Bermain Game");
+        System.out.println("MAKES\t\t: Indomie");
+        System.out.println("MIKES\t\t: Kopi");
+    }
+    
+}
